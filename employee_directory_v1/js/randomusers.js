f5b8b740dd2fb7a33f32769324a2e8673884ccf8
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-04-25 10:08:54 -0500

    -AJAX request to randomuser.api created. -12 list items with initial employee data created

---
+++ employee_directory_v1/js/randomusers.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+  $.ajax({
+    url: 'https://randomuser.me/api/?results=12',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+      var employeeHTML = '<ul class="clearfix">';
+      $.each(data.results, function(i, employee) {
+        employeeHTML += '<li><a href="'+ employee.picture.large + '" ';
+        employeeHTML += 'rel="lightbox"</a>';
+        employeeHTML += '<img src="' + employee.picture.large + '" width="150" height="150"';
+        employeeHTML += '</a>'
+        employeeHTML += `<h4>${employee.name.first} ${employee.name.last}</h4></li>`;
+      }); //end each
+      employeeHTML += '</ul>';
+      $('.employees').html(employeeHTML);
+    }
+  }); //end ajax
+
+}); //end ready
