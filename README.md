ad6169b2054b1f4b2c337d4fa8da46894240b54a
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-04-26 16:03:45 -0500

    -readme credits updated -renamed h2 header -updated css properties -js updated to reflect full grid information from api

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Use-a-Public-API-to-Create-an-Employee-Directory
-Using the Random Generator API to retrieve data for 12 random employees, and using that data to build a prototype for an employee directory. 
+Using the Random Generator API to retrieve data for 12 random employees, and using that data to build a prototype for an employee directory.
+
+Lightbox effect generated via http://leandrovieira.com/projects/jquery/lightbox/
--- employee_directory_v1/js/randomusers.js
+++ employee_directory_v1/js/randomusers.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
 
   $.ajax({
-    url: 'https://randomuser.me/api/?results=12',
+    url: 'https://randomuser.me/api/?results=12&nat=us',
     dataType: 'json',
     success: function(data) {
       console.log(data);
@@ -12,8 +12,11 @@ $(document).ready(function () {
         employeeHTML += '<li><a href="'+ employee.picture.large + '" ';
         employeeHTML += 'rel="lightbox"</a>';
         employeeHTML += '<img src="' + employee.picture.large + '" width="150" height="150"';
-        employeeHTML += '</a>'
-        employeeHTML += `<h4>${employee.name.first} ${employee.name.last}</h4></li>`;
+
+        employeeHTML += `<h4><p class="name">${employee.name.first} ${employee.name.last}</p>`;
+        employeeHTML += `<p class="email">${employee.email}</p>`;
+        employeeHTML += `<p class="city">${employee.location.city}, ${employee.location.state}</p></h4>`;
+        employeeHTML += '</a></li>'
       }); //end each
       employeeHTML += '</ul>';
       $('.employees').html(employeeHTML);
